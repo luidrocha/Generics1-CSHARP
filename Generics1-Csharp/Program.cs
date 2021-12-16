f20e61ab42b03dff0f18812146921d1e4ce752ca
Luidrocha <luidrocha@yahoo.com.br>
2021-12-16 09:44:23 -0300

    Lista numeros basicos com Servico de impressao - CSHARP

---
+++ Generics1-Csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Generics1_Csharp.Service;
+
+namespace Generics1_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService print = new PrintService();
+
+            Console.Write(" Entra com a quantidade de Valores a serem lidos: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int x =0; x<qtd; x++ )
+            {
+
+                Console.Write($"Digite {x}º valor: ");
+                int val = int.Parse(Console.ReadLine());
+
+                print.AddValor(val);
+
+            }
+
+            Console.WriteLine("Primeiro Elemento: " + print.Primeiro());
+
+            Console.WriteLine("Todos os Elemento: ");
+             print.Print();
+
+        }
+    }
+}
---
+++ Generics1-Csharp/Service/PrintService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1_Csharp.Service
+{
+    class PrintService
+    {
+        private int[] _valor = new int[10];
+        private int _contador = 0;
+
+
+        public void AddValor(int valor)
+        {
+            if (_contador == 10)
+            {
+                throw new InvalidOperationException("Serviço de impressão esta cheio, somente 10 Elementos");
+            }
+
+            _valor[_contador] = valor;
+            _contador++;
+
+        }
+
+        public int Primeiro()
+        {
+            if (_contador == 0)
+
+            {
+                new InvalidOperationException("Serviço de impressão está Vazio!!");
+
+            }
+            return _valor[0];
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Valores armazenados no Vetor");
+
+            Console.Write("[");
+            
+            for (int i = 0; i < _contador; i++)
+            {
+
+                Console.Write(_valor[i] + ", ");
+            }
+            Console.Write("]");
+
+
+
+        }
+    }
+}
